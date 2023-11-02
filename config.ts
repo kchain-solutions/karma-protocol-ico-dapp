22a93ce6e517b6412f7b89f9585af28e6b84933a
Valerio Mellini <valerio@kchain.solutions>
2023-11-02 17:57:45 +0100

    chore: setup web3 connection

---
+++ config.ts
@@ -0,0 +1,26 @@
+// Sets if the example should run locally or on chain
+export enum Chain {
+  POLYGON,
+  MAINNET,
+  SEPOLIA,
+}
+
+// Inputs that configure this example to run
+interface ExampleConfig {
+  chain: Chain
+  rpc: {
+    polygon: string
+    mainnet: string
+    sepolia: string
+  }
+}
+
+// Example Configuration
+export const CurrentConfig: ExampleConfig = {
+  chain: Chain.MAINNET,
+  rpc: {
+    polygon: '',
+    mainnet: '',
+    sepolia: '',
+  },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
-import { AppProps } from 'next/app'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Connector } from '@web3-react/types'
+import React, { ReactNode, useEffect } from 'react'
+
+import { ConnectionType, getConnection, PRIORITIZED_CONNECTORS } from '../utils/connections'
+import { AppProps } from 'next/app';
+
+
+async function connect(connector: Connector) {
+  try {
+    if (connector.connectEagerly) {
+      await connector.connectEagerly()
+    } else {
+      await connector.activate()
+    }
+  } catch (error) {
+    console.debug(`web3-react eager connection error: ${error}`)
+  }
+}
+
+
+const connectEagerly = async () => {
+  await connect(getConnection(ConnectionType.NETWORK).connector)
+  await connect(getConnection(ConnectionType.GNOSIS_SAFE).connector)
+}
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    connectEagerly()
+  }, [])
+
   return (
     <div className="app-container">
-      <p> Header </p>
-      <Component {...pageProps} />
-      <p> Footer</p>
+      <Web3ReactProvider connectors={Object.values(PRIORITIZED_CONNECTORS).map((connector) => [connector.connector, connector.hooks])}>
+        <p>Header</p>
+        <Component {...pageProps} />
+        <p>Footer</p>
+      </Web3ReactProvider>
     </div>
-  )
+  );
 }
-
 
 export default MyApp
