acc58b68faea613189220a04b91f02360d6e7f4d
Valerio Mellini <valerio@kchain.solutions>
2023-11-05 07:23:56 +0000

    chore: developing investing form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from 'components/templates/Header'
-import Ico from 'components/organisms/Ico'
+import Ico from 'components/templates/Ico'
 
 
 const Index = () => {
 	return (
 		<>
-			<Header /><Ico />
+			<Header />
+			<Ico />
 		</>
 	)
     
